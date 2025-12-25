675c88559aefca12a8bdcb8e2672fd38be66e96c
JohnOluB <adebayodeolu90@gmail.com>
2025-12-25 07:02:02 +0100

    fix: add theme button functionality

--- Issue tracker/main.js
+++ Issue tracker/main.js
@@ -2,8 +2,11 @@
 const themeButton = document.querySelector("#theme-button");
 
 //Event listener
-
 themeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    themeButton.textContent = "Light mode";
+  } else {
+    themeButton.textContent = "Dark mode";
+  }
 });
-
