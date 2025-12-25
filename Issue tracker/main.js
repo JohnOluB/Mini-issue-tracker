a7cc8772ec61c259b23a3911137e4f092a2b034d
JohnOluB <adebayodeolu90@gmail.com>
2025-12-25 10:00:15 +0100

    feat: create issue functionality

--- Issue tracker/main.js
+++ Issue tracker/main.js
@@ -1,7 +1,11 @@
 //Query selectors
 const themeButton = document.querySelector("#theme-button");
 const mainContainer = document.querySelector(".main-container");
-
+const issueTitle = document.querySelector("#issue-title");
+const issueBox = document.querySelector("#issue-box");
+const saveButton = document.querySelector("#save-button");
+const cancelButton = document.querySelector("#cancel-button");
+const createIssue = document.querySelector("#create-issue");
 //Event listener
 themeButton.addEventListener("click", () => {
   mainContainer.classList.toggle("dark-theme");
@@ -11,3 +15,6 @@ themeButton.addEventListener("click", () => {
     themeButton.textContent = "Dark mode";
   }
 });
+
+createIssue.addEventListener("click", ()=>{
+})
