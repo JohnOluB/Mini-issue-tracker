cfdb4f3f80753f3feecd33f44abfee8585721d99
JohnOluB <adebayodeolu90@gmail.com>
2025-12-25 07:16:53 +0100

    fix: theme button functionality

--- Issue tracker/main.js
+++ Issue tracker/main.js
@@ -1,10 +1,11 @@
 //Query selectors
 const themeButton = document.querySelector("#theme-button");
+const mainContainer = document.querySelector(".main-container");
 
 //Event listener
 themeButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")) {
+  mainContainer.classList.toggle("dark-theme");
+  if (mainContainer.classList.contains("dark-theme")) {
     themeButton.textContent = "Light mode";
   } else {
     themeButton.textContent = "Dark mode";
