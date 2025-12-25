092f2c1060fe380c99fe1bcaee31e7dda6cef1d1
JohnOluB <adebayodeolu90@gmail.com>
2025-12-25 10:31:22 +0100

    feat: add issues array

--- Issue tracker/main.js
+++ Issue tracker/main.js
@@ -1,3 +1,18 @@
+const issues = [
+  {
+    title: "Hunger",
+    description: "I haven't eaten today",
+    status: "open",
+    priority: "high",
+  },
+  {
+    title: "I'm bored",
+    description: "I am feeling bored",
+    status: "open",
+    priority: "medium",
+  },
+];
+
 //Query selectors
 const themeButton = document.querySelector("#theme-button");
 const mainContainer = document.querySelector(".main-container");
@@ -6,6 +21,7 @@ const issueBox = document.querySelector("#issue-box");
 const saveButton = document.querySelector("#save-button");
 const cancelButton = document.querySelector("#cancel-button");
 const createIssue = document.querySelector("#create-issue");
+const issuesList = document.querySelector("#issues-list");
 //Event listener
 themeButton.addEventListener("click", () => {
   mainContainer.classList.toggle("dark-theme");
@@ -16,5 +32,8 @@ themeButton.addEventListener("click", () => {
   }
 });
 
-createIssue.addEventListener("click", ()=>{
-})
+createIssue.addEventListener("click", () => {
+  issue;
+});
+
+issuesList.addEventListener("click", (e) => {});
